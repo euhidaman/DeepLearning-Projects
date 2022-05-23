22311d18605e9c7bbd6c04092d67804dbe59d3f3
euhidaman <aman.derax20@gmail.com>
2022-05-23 23:25:08 +0530

    Created ANN, and showed prediction results

--- CustomerChurnPrediction/script.py
+++ CustomerChurnPrediction/script.py
@@ -1,8 +1,17 @@
 # %%
+# Importing Necessary libraries and modules
+import seaborn as sn
+from sklearn.metrics import confusion_matrix, classification_report
+from tensorflow import keras
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 
+# %%
+# Reading and displaying data from the CSV file
 df = pd.read_csv('WA_Fn-UseC_-Telco-Customer-Churn.csv')
 df.sample(5)
 
@@ -88,3 +97,71 @@ df2.columns
 df2.sample(3)
 
 # %%
+# Scale large data values between 0 and 1
+cols_to_scale = ['tenure', 'MonthlyCharges', 'TotalCharges']
+
+scaler = MinMaxScaler()
+df2[cols_to_scale] = scaler.fit_transform(df2[cols_to_scale])
+
+# %%
+for col in df2:
+    print(f'{col}: {df2[col].unique()}')
+
+# %%
+# Splitting data into, train and test data
+X = df2.drop('Churn', axis='columns')
+y = df2['Churn']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=5)
+
+# %%
+# Building the ANN model
+model = keras.Sequential([
+    keras.layers.Dense(20, input_shape=(26,), activation='relu'),
+    keras.layers.Dense(15, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid'),
+])
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(X_train, y_train, epochs=65)
+
+# %%
+# Evaluate model
+model.evaluate(X_test, y_test)
+
+# %%
+# Predict using the model
+yp = model.predict(X_test)
+yp[:5]
+
+# %%
+y_test[:5]
+
+# %%
+y_pred = []
+for element in yp:
+    if element > 0.5:
+        y_pred.append(1)
+    else:
+        y_pred.append(0)
+
+y_pred[:10]
+
+# %%
+# Print classification report
+print(classification_report(y_test, y_pred))
+
+# %%
+# Print confusion matrix
+cm = tf.math.confusion_matrix(labels=y_test, predictions=y_pred)
+
+plt.figure(figsize=(10, 7))
+sn.heatmap(cm, annot=True, fmt='d')
+plt.xlabel('Predicted')
+plt.ylabel('Truth')
